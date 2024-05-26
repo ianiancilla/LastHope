cb7d1d7dbd4c89b2c5d9f4a25f4d72c59830709a
SLED\Iani <marzia.faustinelli@gmail.com>
2024-05-26 12:15:57 +0200

    add noise to asteroid movement

--- Assets/Scripts/Helpers.cs
+++ Assets/Scripts/Helpers.cs
@@ -14,4 +14,12 @@ public static class Helpers
             ChangeLayersRecursively(child.gameObject, layer);
         }
     }
+
+    public static float Remap (float value, float minOriginalRange, float maxOriginalRange, float minTargetRange, float maxTargetRange)
+    {
+        float range1 = maxOriginalRange - minOriginalRange;
+        float range2 = maxTargetRange - minTargetRange;
+
+        return ((value - minOriginalRange) / range1 * range2) + minTargetRange;
+    }
 }
