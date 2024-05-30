e08367b70a46b3474c7bd96480e94076fe288240
SLED\Iani <marzia.faustinelli@gmail.com>
2024-05-30 17:32:11 +0200

    main menu skeleton, with options and credits. Volume settings work

---
+++ Assets/Scripts/UI/MainMenuNavigation.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class MainMenuNavigation : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject optionsMenu;
+    [SerializeField] private GameObject creditsMenu;
+
+    private void Start()
+    {
+        GoToMainMenu();
+    }
+
+    public void GoToMainMenu()
+    {
+        optionsMenu.SetActive(false);
+        creditsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+    public void GoToOptions()
+    {
+        mainMenu.SetActive(false);
+        creditsMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+    public void GoToCredits()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+    }
+}
